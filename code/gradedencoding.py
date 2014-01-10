6663015b5eb62288dbf376bb2478dad8763d841e
amaloz <amalozemoff1@gmail.com>
2014-01-10 17:04:21 -0500

    removed global parameters plus other changes

--- code/gradedencoding.py
+++ code/gradedencoding.py
@@ -3,32 +3,6 @@
 from sage.all import *
 import sys
 
-LAMBDA = 16
-KAPPA = 2
-
-ALPHA = LAMBDA
-BETA = LAMBDA
-RHO = LAMBDA
-MU = RHO + ALPHA + LAMBDA
-RHOf = KAPPA * (MU + RHO + ALPHA + 2) + RHO
-ETA = RHOf + ALPHA + 2 * BETA + LAMBDA + 8
-NU = ETA - BETA - RHOf - LAMBDA - 3
-N = ETA * log(LAMBDA, base=2)
-
-
-def print_params():
-    print('Parameters:')
-    print('  Lambda: %d' % LAMBDA)
-    print('  Kappa: %d' % KAPPA)
-    print('  Alpha: %d' % ALPHA)
-    print('  Beta: %d' % BETA)
-    print('  Eta: %d' % ETA)
-    print('  Mu: %d' % MU)
-    print('  Nu: %d' % NU)
-    print('  Rho: %d' % RHO)
-    print('  Rhof: %d' % RHOf)
-    print('  N: %d' % N)
-
 
 def logger(s, end='\n'):
     if end == '':
@@ -39,11 +13,39 @@ def logger(s, end='\n'):
 
 
 class GradedEncoding(object):
-    def __init__(self):
-        logger('Generating %d %d-bit primes p_i' % (N, ETA), end='')
+    def set_params(self, secparam, kappa):
+        self.secparam = secparam
+        self.kappa = kappa
+        self.alpha = secparam
+        self.beta = secparam
+        self.rho = secparam
+        self.mu = self.rho + self.alpha + self.secparam
+        self.rho_f = self.kappa * (self.mu + self.rho + self.alpha + 2) + self.rho
+        self.eta = self.rho_f + self.alpha + 2 * self.beta + self.secparam + 8
+        self.nu = self.eta - self.beta - self.rho_f - self.secparam - 3
+        self.n = self.eta * log(self.secparam, base=2)
+
+    def print_params(self):
+        print('Parameters:')
+        print('  Lambda: %d' % self.secparam)
+        print('  Kappa: %d' % self.kappa)
+        print('  Alpha: %d' % self.alpha)
+        print('  Beta: %d' % self.beta)
+        print('  Eta: %d' % self.eta)
+        print('  Mu: %d' % self.mu)
+        print('  Nu: %d' % self.nu)
+        print('  Rho: %d' % self.rho)
+        print('  Rhof: %d' % self.rho_f)
+        print('  N: %d' % self.n)
+
+    def __init__(self, secparam=32, kappa=1):
+        self.set_params(secparam, kappa)
+        self.print_params()
+
+        logger('Generating %d-bit primes p_i' % self.eta, end='')
         self.ps = []
-        for _ in range(N):
-            self.ps.append(random_prime((1 << ETA) - 1))
+        for _ in range(self.n):
+            self.ps.append(random_prime((1 << self.eta) - 1))
             logger('.', end='')
         logger('')
         logger('Computing x0')
@@ -56,53 +58,26 @@ class GradedEncoding(object):
                 break
             except:
                 pass
-        logger('Generating %d %d-bit primes g_i' % (N, ALPHA), end='')
+        logger('Generating %d-bit primes g_i' % self.alpha, end='')
         self.gs = []
-        for _ in range(N):
-            self.gs.append(random_prime((1 << ALPHA) - 1))
+        for _ in range(self.n):
+            self.gs.append(random_prime((1 << self.alpha) - 1))
             logger('.', end='')
         logger('')
-        # logger('Generating H')
-        # nrows, ncols = floor(N / 2.0), ceil(N / 2.0)
-        # MS = MatrixSpace(ZZ, nrows, ncols)
-        # Ifloor = MS(identity_matrix(nrows))
-        # Iceil = MS(identity_matrix(ncols))
-        # Z = MS(zero_matrix(nrows, ncols))
-        # b = max((floor(BETA / ceil(log(1 + ncols, base=2))), 1))
-        # Hs = []
-        # for _ in range(b):
-        #     A = MS(random_matrix(Zmod(3), nrows, ncols)) - \
-        #         ones_matrix(nrows, ncols)
-        #     HA = block_matrix(ZZ, 2, 2, [Ifloor, A, Iceil, Z])
-        #     if randint(0, 1) == 1:
-        #         HA = HA.transpose()
-        #     Hs.append(HA)
-        # H = matrix(reduce(operator.mul, Hs))
-        # print('Checking singularity...')
-        # assert not H.is_singular(), "H is singular!"
-        # print('Checking norms...')
-        # assert numpy.linalg.norm(H, ord=numpy.inf) <= (1 << BETA)
-        # assert numpy.linalg.norm(H.inverse(), ord=numpy.inf) <= (1 << BETA)
         logger('Generating zero test element')
-        zk = power_mod(self.z, KAPPA, self.x0)
+        zk = power_mod(self.z, self.kappa, self.x0)
         x0ps = [self.x0 / p for p in self.ps]
         gsis = [inverse_mod(g, p) for g, p in zip(self.gs, self.ps)]
-        self.pzt = sum(randint(0, (1 << BETA) - 1) * zk * gsis[i] * x0ps[i]
-                       for i in range(N))
-        # logger('Generating zero test vector', end='')
-        # self.pzts = []
-        # for j in range(N):
-        #     v = sum(H[i][j] * zk * gsis[i] * x0ps[i] for i in range(N))
-        #     self.pzts.append(v % self.x0)
-        #     logger('.', end='')
-        # logger('')
+        self.pzt = sum(randint(0, (1 << self.beta) - 1) * zk * gsis[i] * x0ps[i]
+                       for i in range(self.n))
 
     def encode(self, msg):
-        ms = [0 for _ in range(N)]
+        logger('Encoding value %d' % msg)
+        ms = [0 for _ in range(self.n)]
         ms[0] = msg
         assert msg < self.gs[0], "Message must be smaller than g_0"
-        logger('Generating %d random %d-bit integers r_i' % (N, RHOf))
-        rs = [random_prime((1 << RHOf) - 1) for _ in range(N)]
+        logger('Generating random %d-bit integers r_i' % self.rho_f)
+        rs = [random_prime((1 << self.rho_f) - 1) for _ in range(self.n)]
         logger('Generating elements for CRT')
         elems = [(r * g + m) * self.zinv % p
                  for r, g, m, p in zip(rs, self.gs, ms, self.ps)]
@@ -110,28 +85,45 @@ class GradedEncoding(object):
         return CRT(elems, self.ps)
 
     def is_zero(self, c):
-        omega = self.pzt * c % self.x0
-        return abs(omega) < (self.x0 >> NU)
-        # omega = [c * e % self.x0 for e in self.pzts]
-        # return max(omega) < (self.x0 >> NU)
+        omega = (self.pzt * c) % self.x0
+        return abs(omega) < (self.x0 >> self.nu)
+
+    def add(self, cs):
+        return reduce(operator.add, cs) % self.x0
+
+    def mult(self, cs):
+        return reduce(operator.mul, cs) % self.x0
 
 
 if __name__ == '__main__':
-    print_params()
+    if len(sys.argv) != 2:
+        print("invalid number of arguments")
+        sys.exit(1)
 
-    if len(sys.argv) == 1:
-        inp = [0]
-    else:
-        inp = [int(i) for i in sys.argv[1:]]
+    args = sys.argv[1].split()
+    vals = []
+    ops = []
+    for arg in args:
+        try:
+            vals.append(int(arg))
+        except ValueError:
+            assert arg in ('+', '*'), "invalid argument"
+            ops.append(arg)
 
-    ge = GradedEncoding()
-    cs = []
-    for i in inp:
-        print('---- Encoding value %d' % i)
-        c = ge.encode(i)
-        cs.append(c)
-    r = reduce(operator.mul, cs) % ge.x0
-    if ge.is_zero(r):
+    secparam = 16
+    kappa = ops.count('*') + 1
+
+    ge = GradedEncoding(secparam=secparam, kappa=kappa)
+    encvals = [ge.encode(v) for v in vals]
+    for op in ops:
+        a, b = encvals.pop(0), encvals.pop(0)
+        if op == '+':
+            encvals.insert(0, ge.add([a, b]))
+        else:
+            encvals.insert(0, ge.mult([a, b]))
+    assert len(encvals) == 1
+
+    if ge.is_zero(encvals[0]):
         print('---- Output is ZERO')
     else:
         print('---- Output is something else')
