c4f2b3349db0726cdf675f3586a6ee5a82b3ada7
amaloz <amalozemoff1@gmail.com>
2014-01-10 12:46:53 -0500

    implemented is_zero, among other changes, although it doesn't quite work yet...

--- code/gradedencoding.py
+++ code/gradedencoding.py
@@ -4,32 +4,50 @@ from __future__ import print_function
 from sage.all import *
 import sys
 
-SECPARAM = 4
-KAPPA = 4
+LAMBDA = 16
+KAPPA = 1
 
-VECTOR_DIM = 20 * SECPARAM * KAPPA
-ALPHA = SECPARAM
-ETA = 10 * SECPARAM * KAPPA
-RHO = SECPARAM
+ALPHA = LAMBDA
+BETA = LAMBDA
+RHO = LAMBDA
+MU = RHO + ALPHA + LAMBDA
+RHOf = KAPPA * (MU + RHO + ALPHA + 2) + RHO
+ETA = RHOf + ALPHA + 2 * BETA + LAMBDA + 8
+NU = ETA - BETA - RHOf - LAMBDA - 3
+N = ETA * log(LAMBDA, base=2)
 
 
-def log(s, **kwargs):
+def print_params():
+    print('Parameters:')
+    print('  Lambda: %d' % LAMBDA)
+    print('  Kappa: %d' % KAPPA)
+    print('  Alpha: %d' % ALPHA)
+    print('  Beta: %d' % BETA)
+    print('  Eta: %d' % ETA)
+    print('  Mu: %d' % MU)
+    print('  Nu: %d' % NU)
+    print('  Rho: %d' % RHO)
+    print('  Rhof: %d' % RHOf)
+    print('  N: %d' % N)
+
+
+def logger(s, **kwargs):
     print(s, **kwargs)
     sys.stdout.flush()
 
 
 class GradedEncoding(object):
     def __init__(self):
-        log('Generating %d %d-bit primes p_i' % (VECTOR_DIM, ETA), end='')
+        logger('Generating %d %d-bit primes p_i' % (N, ETA), end='')
         self.ps = []
-        for _ in range(VECTOR_DIM):
-            self.ps.append(random_prime(2**ETA - 1))
-            log('.', end='')
-        log('')
-        log('Computing x0', end='')
+        for _ in range(N):
+            self.ps.append(random_prime((1 << ETA) - 1))
+            logger('.', end='')
+        logger('')
+        logger('Computing x0', end='')
         self.x0 = reduce(operator.mul, self.ps)
-        log('')
-        log('Generating z', end='')
+        logger('')
+        logger('Generating z', end='')
         while True:
             self.z = randint(0, self.x0)
             try:
@@ -37,43 +55,67 @@ class GradedEncoding(object):
                 break
             except:
                 pass
-        log('')
-        log('Generating %d %d-bit primes g_i' % (VECTOR_DIM, ALPHA), end='')
+        logger('')
+        logger('Generating %d %d-bit primes g_i' % (N, ALPHA), end='')
         self.gs = []
-        for _ in range(VECTOR_DIM):
-            self.gs.append(random_prime(2**ALPHA - 1))
-            log('.', end='')
-        log('')
+        for _ in range(N):
+            self.gs.append(random_prime((1 << ALPHA) - 1))
+            logger('.', end='')
+        logger('')
+        logger('Generating H', end='')
+        nrows, ncols = floor(N / 2.0), ceil(N / 2.0)
+        MS = MatrixSpace(ZZ, nrows, ncols)
+        Ifloor = MS(identity_matrix(nrows))
+        Iceil = MS(identity_matrix(ncols))
+        Z = MS(zero_matrix(nrows, ncols))
+        b = max((floor(BETA / ceil(log(1 + ncols, base=2))), 1))
+        Hs = []
+        for _ in range(b):
+            A = MS(random_matrix(Zmod(3), nrows, ncols)) - \
+                ones_matrix(nrows, ncols)
+            HA = block_matrix(ZZ, 2, 2, [Ifloor, A, Iceil, Z])
+            if randint(0, 1) == 1:
+                HA = HA.transpose()
+            Hs.append(HA)
+        H = reduce(operator.mul, Hs)
+        logger('')
+        logger('Generating zero test vector', end='')
+        self.pzts = []
+        zk = power_mod(self.z, KAPPA, self.x0)
+        x0ps = [self.x0 / p for p in self.ps]
+        gsis = [inverse_mod(self.gs[i], self.ps[i])
+                for i in range(len(self.gs))]
+        for j in range(N):
+            v = sum(H[i][j] * zk * gsis[i] * x0ps[i] for i in range(N))
+            self.pzts.append(v % self.x0)
+            logger('.', end='')
+        logger('')
 
     def encode(self, m):
-        ms = [0 for _ in range(VECTOR_DIM)]
+        ms = [0 for _ in range(N)]
         ms[0] = m
-        log('Generating %d random %d-bit integers r_i' % (VECTOR_DIM, RHO),
-            end='')
+        logger('Generating %d random %d-bit integers r_i' % (N, RHO), end='')
         rs = []
-        for _ in range(VECTOR_DIM):
-            rs.append(random_prime(2**RHO - 1))
-            log('.', end='')
-        log('')
-        log('Generating elements for CRT', end='')
-        elems = []
-        for i in range(VECTOR_DIM):
-            elems.append(((rs[i] * self.gs[i] + ms[i]) * self.zinv) % self.ps[i])
-        log('')
-        log('Finding c', end='')
+        for _ in range(N):
+            rs.append(random_prime((1 << RHO) - 1))
+            logger('.', end='')
+        logger('')
+        logger('Generating elements for CRT', end='')
+        elems = [(rs[i] * self.gs[i] + ms[i]) * self.zinv % self.ps[i]
+                 for i in range(len(ms))]
+        logger('')
+        logger('Finding c', end='')
         c = CRT(elems, self.ps)
-        log('')
+        logger('')
         return c
+
+    def is_zero(self, c):
+        omega = [c * e % self.x0 for e in self.pzts]
+        return max(omega) < (self.x0 >> NU)
 
 
 if __name__ == '__main__':
-    print('Parameters:')
-    print('  Security Parameter: %d' % SECPARAM)
-    print('  Kappa: %d' % KAPPA)
-    print('  Vector Dimension: %d' % VECTOR_DIM)
-    print('  Alpha: %d' % ALPHA)
-    print('  Eta: %d' % ETA)
-    print('  Rho: %d' % RHO)
+    print_params()
 
     if len(sys.argv) == 1:
         inp = 0
@@ -84,3 +126,7 @@ if __name__ == '__main__':
     print('---- Encoding value %d' % inp)
     c = ge.encode(inp)
     print('---- Encoded value = %d' % c)
+    if ge.is_zero(c):
+        print('---- Output is *0*')
+    else:
+        print('---- Output is *1*')
