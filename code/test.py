cc904ed4f4e4b8b6a1cdf550541db989e992019f
amaloz <amalozemoff1@gmail.com>
2014-01-16 12:32:35 -0500

    renamed circ2bp_test.py -> test.py

---
+++ code/test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import branchingprogram
+import os
+
+testdir = 'circuits'
+
+if __name__ == "__main__":
+    for circuit in os.listdir('circuits'):
+        path = os.path.join(testdir, circuit)
+        testcases = {}
+        print('Testing %s: ' % circuit, end='')
+        with open(path) as f:
+            for line in f:
+                if line.startswith('#'):
+                    if 'TEST' in line:
+                        _, _, inp, outp = line.split()
+                        testcases[inp] = int(outp)
+                else:
+                    continue
+        if len(testcases) == 0:
+            print()
+            continue
+        bp = branchingprogram.BranchingProgram(path, type='circuit')
+        bp.obliviate()
+        bp.randomize()
+        failed = False
+        for k, v in testcases.items():
+            if bp.evaluate(k) != v:
+                print('\x1b[31mFail\x1b[0m (%s != %d) ' % (k, v), end='')
+                failed = True
+        if failed:
+            print()
+        else:
+            print('\x1b[32mPass\x1b[0m')
