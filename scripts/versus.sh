2620bb7067f160d67d201124179e2907e62b3fd6
amaloz <amaloz@cs.umd.edu>
2014-05-06 16:14:39 -0400

    updated versus.sh

--- scripts/versus.sh
+++ scripts/versus.sh
@@ -17,17 +17,32 @@ pushd $CIRCUIT_DIR
 ./point.py $POINT
 popd
 
-secparam=48
+secparam=24
 
 dir="$LOG_DIR/versus.$POINT.$secparam"
 mkdir -p $dir
 
 circuit="point-$POINT.circ"
-echo "* Running $circuit with security parameter $secparam"
+echo "* Running $circuit with security parameter $secparam (old)"
 $SAGE $CODE_DIR/indobf/run.py obf \
     --load-circuit $CIRCUIT_DIR/$circuit \
     --secparam $secparam \
     --old \
+    --verbose 2>&1 | tee $dir/$circuit-$secparam-obf-time.log
+obf=$circuit.obf.$secparam
+du --bytes $CIRCUIT_DIR/$obf/* \
+    | tee $dir/$circuit-$secparam-obf-size.log
+eval=`$PYTHON -c "print('0' * $POINT)"`
+$SAGE $CODE_DIR/indobf/run.py obf \
+    --load-obf $CIRCUIT_DIR/$obf \
+    --eval $eval \
+    --verbose 2>&1 | tee $dir/$circuit-$secparam-eval-time.log
+rm -rf $CIRCUIT_DIR/$circuit.obf.$secparam
+
+echo "* Running $circuit with security parameter $secparam (new)"
+$SAGE $CODE_DIR/indobf/run.py obf \
+    --load-circuit $CIRCUIT_DIR/$circuit \
+    --secparam $secparam \
     --verbose 2>&1 | tee $dir/$circuit-$secparam-obf-time.log
 obf=$circuit.obf.$secparam
 du --bytes $CIRCUIT_DIR/$obf/* \
