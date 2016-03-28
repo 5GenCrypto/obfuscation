a2a5cacac74ea5d0fa695fb96f2e7b681576e7ef
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-03-28 11:44:15 -0400

    python script for creating json point functions

---
+++ code/circuits/point-json.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import random, subprocess, sys
+
+def usage():
+    print('Usage: point-json.py <bitlength>')
+    sys.exit(1)
+
+def random_bitstring(bitlength):
+    r = random.randint(0, 2 ** bitlength - 1)
+    bits = bin(r)[2:]
+    return '0' * (bitlength - len(bits)) + bits
+
+def run(lst):
+    print('%s' % ' '.join(lst))
+    return subprocess.call(lst)
+
+def binary_point(bitlength):
+    secret = random_bitstring(bitlength)
+    array = '['
+    for x in range(1, bitlength + 1):
+        array += '"%d", ' % x
+    array += '"%d"]' % (bitlength + 1)
+    lst = ['cryfsm', '-e', "(==) 0b%s" % secret, '-v', "\_ -> True", '-g',
+           array, '-o', 'point-%d.json' % bitlength]
+    run(lst)
+    with open('point-%d.json' % bitlength, 'r') as f:
+        line = f.read()
+    with open('point-%d.json' % bitlength, 'w') as f:
+        f.write('# TEST %s 1\n' % secret)
+        for _ in xrange(5):
+            test = random_bitstring(bitlength)
+            if test != secret:
+                f.write('# TEST %s 0\n' % test)
+        f.write(line)
+
+def main(argv):
+    if len(argv) != 2:
+        usage()
+    try:
+        bitlength = int(argv[1])
+    except ValueError:
+        usage()
+
+    binary_point(bitlength)
+
+if __name__ == '__main__':
+    try:
+        main(sys.argv)
+    except KeyboardInterrupt:
+        pass
