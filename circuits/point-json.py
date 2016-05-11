666dbdfd497f7b4e7570eb7a68724be2719af77d
Alex J. Malozemoff <amaloz@cs.umd.edu>
2016-05-11 00:16:44 -0400

    fix up cryfsm point function generation

--- circuits/point-json.py
+++ circuits/point-json.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python2
 
 from __future__ import print_function
-import os, random, subprocess, sys
+import math, os, random, subprocess, sys
 
 def usage():
-    print('Usage: point-json.py <bitlength> <base>')
+    print('Usage: point-json.py <length> <base>')
     sys.exit(1)
 
 def digit_to_char(digit):
@@ -25,47 +25,55 @@ def dary_repr(number, d, n):
     repr = (['0'] * (n - len(repr))) + repr
     return "".join(repr)
 
-def random_bitstring(bitlength, base):
-    num = random.randint(0, 2 ** bitlength - 1)
-    repr = dary_repr(num, base, bitlength)
-    bits = bin(num)[2:]
-    return '0' * (bitlength - len(bits)) + bits
+# turns a "10" in any base to a 001 000
+def digit_dary_repr(num_str, d):
+    L = []
+    for x in num_str:
+        L.append(format(int(x), 'b').zfill(int(math.ceil(math.log(d, 2)))))
+    return "".join(L)
 
 def run(lst):
     # print('%s' % ' '.join(lst))
     with open(os.devnull, 'w') as fnull:
         return subprocess.call(lst, stdout=fnull, stderr=fnull)
 
-def point(bitlength, base):
-    secret = random_bitstring(bitlength, base)
+def point(length, base):
+    secret = random.randint(0, base ** length - 1)
+    repr = dary_repr(secret, base, length)
+    base_2_len = len(str_base(base-1, 2))
+    inp = ''.join([dary_repr(int(i), 2, base_2_len) for i in repr])
+    print(base ** length)
     lst = []
-    for i in xrange(bitlength):
-        lst.append('"%d"' % (i))
+    for i in xrange(length):
+        for b in xrange(base-1):
+            lst.append('"%d"' % i)
     str = '[' + ','.join(lst) + ']'
-    fname = 'point-%d-%d.json' % (bitlength, base)
-    lst = ['cryfsm', '-e', "(==) 0b%s" % secret, '-v', "\_ -> True", '-g',
+    fname = 'point-%d-%d.json' % (length, base)
+    lst = ['cryfsm', 'util.cry', '-e', "(==) 0b%s" % inp,
+           '-v', "adjacentConstantBase `{base=%d}" % base, '-g',
            "%s" % str, '-o', fname]
     run(lst)
     with open(fname, 'r') as f:
         line = f.read()
     with open(fname, 'w') as f:
-        f.write('# TEST %s 1\n' % secret)
+        f.write('# TEST %s 1\n' % repr)
         for _ in xrange(5):
-            test = random_bitstring(bitlength, base)
+            test = random.randint(0, base ** length - 1)
+            repr = dary_repr(test, base, length)
             if test != secret:
-                f.write('# TEST %s 0\n' % test)
+                f.write('# TEST %s 0\n' % repr)
         f.write(line)
 
 def main(argv):
     if len(argv) != 3:
         usage()
     try:
-        bitlength = int(argv[1])
+        length = int(argv[1])
         base = int(argv[2])
     except ValueError:
         usage()
 
-    point(bitlength, base)
+    point(length, base)
 
 if __name__ == '__main__':
     try:
