6fb5a25fdce58c8593fb8aa702ffc463b215119f
Alex J. Malozemoff <amaloz@galois.com>
2017-09-14 14:19:23 -0700

    remove loop.sh

--- loop.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-RESULT=0
-while [ "$RESULT" -eq "0" ]; do
-    ./obfuscator obf --test circuits/point-4.circ -v --nthreads 1 --ncores 1 --mmap CLT --secparam 8
-    RESULT=$?
-done
